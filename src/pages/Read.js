e4ff93591979d4f58b25289dd6570af9eb8218e3
amd200 <72322072+amd200@users.noreply.github.com>
2024-05-28 09:16:22 +0300

    Update Read.js

--- src/pages/Read.js
+++ src/pages/Read.js
@@ -3,20 +3,16 @@ import { useParams } from "react-router-dom";
 import { getReciterAudios, getSurah } from "../redux/actions/MuslimAction";
 import React, { useEffect, useState } from "react";
 import Audio from "../Components/Audio/Audio";
+import elnegm from "../assets/audios/سورة النجم بمقام النهاوند.mp3";
 
 function Read() {
   const [ayahs, setAyahs] = useState([]);
-  let { surahId, recitersId } = useParams();
-  if (surahId < 10) {
-    surahId = `00${surahId}`;
-  } else if (surahId < 100) {
-    surahId = `0${surahId}`;
-  } else {
-    surahId = `${surahId}`;
-  }
+  const [audioSrc, setAudioSrc] = useState("./a.mp3"); 
+  const { surahId, recitersId } = useParams();
   const dispatch = useDispatch();
   const data = useSelector((state) => state.surah);
   const serverAudio = useSelector((state) => state.audio);
+
   useEffect(() => {
     dispatch(getSurah());
     dispatch(getReciterAudios(recitersId));
@@ -27,10 +23,26 @@ function Read() {
       setAyahs(data[surahId - 1]);
     }
   }, [data, surahId, serverAudio]);
+
+  useEffect(() => {
+    const specialAudioFiles = {
+      "112": {
+        "53": elnegm,
+      },
+    };
+
+    const specialAudioSrc = specialAudioFiles[recitersId]?.[surahId];
+    if (specialAudioSrc) {
+      setAudioSrc(specialAudioSrc); 
+    } else {
+      setAudioSrc(`${serverAudio}${surahId}.mp3`);
+    }
+  }, [recitersId, surahId, serverAudio, audioSrc]);
+
   return (
     <div className="container mt-4">
       <h3 className="name text-center">سورة {ayahs.name}</h3>
-      <div id="quran" className=" p-3 mt-4">
+      <div id="quran" className="p-3 mt-4">
         <p id="text2">
           {ayahs.verses &&
             ayahs.verses.map((verse) => (
@@ -43,9 +55,10 @@ function Read() {
             ))}
         </p>
       </div>
-      <Audio src={serverAudio + surahId + `.mp3`} />
+      <Audio src={audioSrc} />
     </div>
   );
 }
 
 export default Read;
+

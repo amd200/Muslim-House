cffe39f2beaced7ed2346996c81e59317c8a25e5
amd200 <72322072+amd200@users.noreply.github.com>
2024-06-21 06:16:41 +0300

    Update store.js

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,5 +1,4 @@
 import { applyMiddleware, createStore } from "redux";
 import { MuslimReducer } from "../reducer/MuslimReducer";
 import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-export const store = createStore(MuslimReducer, composeWithDevTools(applyMiddleware(thunk)));
+export const store = createStore(MuslimReducer, applyMiddleware(thunk));

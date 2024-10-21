461f943d913fed4bccc485b59d540bd3287b2d46
amd200 <72322072+amd200@users.noreply.github.com>
2024-10-22 01:03:39 +0300

    Update Stories.jsx

--- src/pages/Stories.jsx
+++ src/pages/Stories.jsx
@@ -6,7 +6,8 @@ function Stories() {
   const dispatch = useDispatch();
   const videos = useSelector((state) => state.youtube);
   const loading = useSelector((state) => state.loading);
-  const [currentVideo, setCurrentVideo] = useState(null); // حالة لتخزين رابط الفيديو الحالي
+  const [currentVideo, setCurrentVideo] = useState(null);
+  const [nameVideo, setNameVideo] = useState("");
 
   useEffect(() => {
     dispatch(youtube());
@@ -16,6 +17,8 @@ function Stories() {
   // تعيين رابط الفيديو الحالي عند النقر على الفيديو
   const handleVideoClick = (videoId) => {
     setCurrentVideo(videoId);
+    setNameVideo(videos.find((video) => video.snippet.resourceId.videoId === videoId).snippet.title);
+
   };
 
   return (
@@ -23,36 +26,42 @@ function Stories() {
       <div className="container">
         <div className="row">
           <div className="col-lg-4">
-            <div className="video-list">
+            <div className="video-list position-relative">
               {loading === false && videos.length >= 1
                 ? videos.map((video, index) => (
-                    <div
-                      className="video"
-                      key={index}
-                      onClick={() =>
-                        handleVideoClick(video.snippet.resourceId.videoId)
-                      } // تعيين رابط الفيديو عند النقر
-                    >
-                      <img
-                        src={video.snippet.thumbnails.default.url}
-                        alt={video.snippet.title}
-                      />
-                      <h3 className="title">{video.snippet.title}</h3>
-                    </div>
-                  ))
-                : "null"}
+                  <div
+                    className="video"
+                    key={index}
+                    onClick={() =>
+                      handleVideoClick(video.snippet.resourceId.videoId)
+                    } // تعيين رابط الفيديو عند النقر
+                  >
+                    <img
+                      src={video.snippet.thumbnails.default.url}
+                      alt={video.snippet.title}
+                    />
+                    <h3 className="title">{video.snippet.title}</h3>
+                  </div>
+                ))
+                : <div class="spinner-border text-success my-5 position-absolute top-50 start-50 translate-middle" role="status">
+                  <span class="visually-hidden">Loading...</span>
+                </div>}
             </div>
           </div>
           <div className="col-lg-8">
             {currentVideo && (
-              <iframe
-                src={`https://www.youtube.com/embed/${currentVideo}`}
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerpolicy="strict-origin-when-cross-origin"
-                allowfullscreen
-              ></iframe>
+              <>
+                <iframe
+                  src={`https://www.youtube.com/embed/${currentVideo}`}
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerpolicy="strict-origin-when-cross-origin"
+                  allowfullscreen
+                ></iframe>
+                <h5 className="mt-2">{nameVideo}</h5>
+              </>
+
             )}
           </div>
         </div>

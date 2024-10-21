35a4617a38882462a7087cca92319bc577a003e6
amd200 <72322072+amd200@users.noreply.github.com>
2024-10-22 01:02:11 +0300

    Update store.js

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,4 +1,4 @@
 import { applyMiddleware, createStore } from "redux";
 import { MuslimReducer } from "../reducer/MuslimReducer";
 import { thunk } from "redux-thunk";
-export const store = createStore(MuslimReducer, applyMiddleware(thun));
+export const store = createStore(MuslimReducer, applyMiddleware(thunk));

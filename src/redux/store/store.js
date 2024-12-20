342eb1eaac02410d37f4aa28aa627029021a3335
amd200 <ahm12d.m@gmail.com>
2024-12-20 15:33:40 +0200

    done

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,5 +1,4 @@
 import { applyMiddleware, createStore } from "redux";
 import { MuslimReducer } from "../reducer/MuslimReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { thunk } from "redux-thunk";
-export const store = createStore(MuslimReducer, composeWithDevTools(applyMiddleware(thunk)));
+export const store = createStore(MuslimReducer, applyMiddleware(thunk));
